390be32488e11d977b05bee1c2676c6e0594d3d8
Faisal Farooq <dfaisal59@gmail.com>
2024-09-05 21:02:10 +0530

    Refactor Medication component to handle cases with no medications

--- src/components/Patient/Medication.jsx
+++ src/components/Patient/Medication.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Medication = ({ patient }) => {
+  if (!patient.medicines) {
+    return <h1>No Medications</h1>;
+  }
   return (
     <div className="bg-white p-4 sm:p-6 rounded-lg ring-1 ring-gray-400 overflow-x-clip">
       <h1 className="text-lg sm:text-xl font-bold mb-4">Medications</h1>
